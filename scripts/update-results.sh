00af465eb550957b20d8dce5aa476cf74fb39f8f
Gokul Jangam <145959824+chenchugokuljangam13@users.noreply.github.com>
2025-05-22 12:04:39 +0530

    Update update-results.sh

--- scripts/update-results.sh
+++ scripts/update-results.sh
@@ -8,13 +8,13 @@ testHistory=$(jq '[.testResults[0].assertionResults[] | {testName: .title, testS
 
 echo "Formatted Test History: $testHistory"
 
-patchPayload=$(jq -n --argjson history "$testHistory" '{ result_summary: $history }')
+resultSummary=$(jq -n --argjson history "$testHistory" '{ result_summary: $history }')
 
-echo "Payload to PATCH: $patchPayload"
+echo "Payload to PATCH: $resultSummary"
 
 curl -X PATCH "$SUPABASE_URL/rest/v1/candidate_assessment?id=eq.${ASSESSMENT_ID}" \
   -H "apikey: $SUPABASE_API_KEY" \
   -H "Authorization: Bearer $SUPABASE_API_KEY" \
   -H "Content-Type: application/json" \
   -H "Prefer: return=representation" \
-  -d "$patchPayload"
+  -d "$resultSummary"
